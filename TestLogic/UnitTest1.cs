b460df1a77c0fe452ee0250d7ef4f6cf5e24e277
Sergey Kobelev <kobelev_s@mail.ru>
2016-01-20 00:08:03 +0400

    Добавлен Reverse только для не nullable

--- TestLogic/UnitTest1.cs
+++ TestLogic/UnitTest1.cs
@@ -9,42 +9,90 @@ namespace TestLogic
     [TestClass]
     public class UnitTest1
     {
+        public static List<string> nullStrList = null;
+        public static List<string> oneStrList;
+        public static List<string> manyStrList;
+        public static List<int> oneIntList;
+        public static List<int> manyIntList;
+
+        [TestInitialize]
+        public void NewLists ()
+        {
+            oneStrList = new List<string>();
+
+            oneStrList.Add("string1");
+            manyStrList = new List<string>();
+            manyStrList.Add("string1");
+            manyStrList.Add("string2");
+            manyStrList.Add("string3");
+            manyStrList.Add("string4");
+
+            oneIntList = new List<int>();
+            oneIntList.Add(1);
+
+            manyIntList = new List<int>();
+            manyIntList.Add(1);
+            manyIntList.Add(2);
+            manyIntList.Add(3);
+            manyIntList.Add(4);
+        }
+
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
         public void TestNullList()
         {
-            List<string> strList = null;
-            strList = strList.MyReverse();
+            nullStrList = nullStrList.MyReverse();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestNullStructList()
+        {
+            var intList = ListStructReverse<int>.MyReverse(null);
         }
 
         [TestMethod]
         public void TestOneOrZeroEllementInList ()
         {
-            List<string> strList = new List<string>();
-            strList.Add("string1");
-            var newList = strList.MyReverse();
+            var newList = oneStrList.MyReverse();
             if (newList.Count == 1)
                 Assert.AreEqual(newList[0],"string1");
         }
 
         [TestMethod]
+        public void TestStructOneOrZeroEllementInList()
+        {
+            var newList = ListStructReverse<int>.MyReverse(oneIntList);
+            if (newList.Count == 1)
+                Assert.AreEqual(newList[0], 1);
+        }
+
+        [TestMethod]
         public void TestMoreThenOneEllementInList()
         {
-            List<string> strList = new List<string>();
-            strList.Add("string1");
-            strList.Add("string2");
-            strList.Add("string3");
-            strList.Add("string4");
+            Assert.AreEqual(manyStrList.Count, 4);
+            var newList = manyStrList.MyReverse();
 
-            Assert.AreEqual(strList.Count, 4);
-            var newList = strList.MyReverse();
-
-            for (int i = strList.Count; i > 0; i--)
+            for (int i = manyStrList.Count; i > 0; i--)
             {
-                Trace.WriteLine(strList[newList.Count  - i] + " : string" + i.ToString());
-                Assert.AreEqual(strList[newList.Count-i], "string" + i.ToString());
+                Trace.WriteLine(manyStrList[newList.Count  - i] + " : string" + i.ToString());
+                Assert.AreEqual(manyStrList[newList.Count-i], "string" + i.ToString());
             }
             
+        }
+
+        [TestMethod]
+        public void TestStructMoreThenOneEllementInList()
+        {
+            Assert.AreEqual(manyIntList.Count, 4);
+            var newList = ListStructReverse<int>.MyReverse(manyIntList);
+
+            for (int i = manyIntList.Count; i > 0; i--)
+            {
+                Trace.WriteLine(newList[newList.Count - i] + " : " + manyIntList[i - 1]);
+                Assert.AreEqual(newList[newList.Count - i], manyIntList[i - 1]);
+            }
+
         }
     }
 }
